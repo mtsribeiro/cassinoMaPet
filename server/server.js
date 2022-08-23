3a4d36149095cd6423ee0f9d4807e0cef8a2358c
Mateus Ribeiro <98188179+mtsribeiro@users.noreply.github.com>
2022-08-23 13:54:16 -0300

    .
    .

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const port = 9000;
 app.use('/', express.static(path.join(__dirname, '../ui')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../ui/index.html'))
+  res.sendFile(path.join(__dirname, '../ui/home/index.html'))
+})
+app.get('/crash', (req, res) => {
+    res.sendFile(path.join(__dirname, '../ui/crash/index.html'))
+})
+app.get('/double', (req, res) => {
+    res.sendFile(path.join(__dirname, '../ui/double/index.html'))
 })
 
 app.listen(port, () => {
---
+++ ui/home/app.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-entraCrash').addEventListener("click", function() {
+    window.location.href = '/crash'
+})
+document.getElementById('btn-entraDouble').addEventListener("click", function() {
+    window.location.href = '/double'
+})
