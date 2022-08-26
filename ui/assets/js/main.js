4cc571826b71dc0e162061f8ad1d47a6bf4254e0
Mateus Ribeiro <98188179+mtsribeiro@users.noreply.github.com>
2022-08-26 11:38:05 -0300

    .
    .

--- ui/assets/js/main.js
+++ ui/assets/js/main.js
@@ -92,9 +92,12 @@ const senhaLogin = document.getElementById('senhaLogin').value
     localStorage.setItem('infologin', JSON.stringify(msg))
 
     $('#MenuLogin').css('display', 'none');
+    $('#ColunaBatePapo').css('display', 'block');
     document.location.reload(true);
 
   } else {
+
+    $('#avisoToast').text('Usuário ou senha não conferem.')
     $('.toast').toast('show');
 
     setTimeout(() => {
@@ -119,13 +122,71 @@ $('#MenuRegistro').css('display', 'none');
 $(document).on('click', '#deslogarLogin', function(e){
 e.preventDefault()
 localStorage.removeItem('infologin');
+$('#ColunaBatePapo').css('display', 'none');
+$('#MenuRegistro').css('display', 'none');
 document.location.reload(true);
-
-$('#MenuLogin').css('display', 'none');
 })
 
-$(document).on('click', '#deslogarLogin', function(e){
-  e.preventDefault()
-  $('#MenuRegistro').css('display', 'none');
-  document.location.reload(true);
+
+
+$(document).on('click', '#checaRegistro', function(e){
+e.preventDefault()
+
+const emailRegistro = document.getElementById('emailRegistroLogin').value
+const senhaRegistro = document.getElementById('senhaRegistroLogin').value
+const nomeRegistro = document.getElementById('nomeRegistroExibicao').value
+
+ $.ajax({
+  url : "/consultaRegistroUsuario",
+  type : 'post',
+  data : {
+       email: emailRegistro,
+       password: senhaRegistro,
+       nome: nomeRegistro
+  }
+})
+.done(function(msg){
+if (msg == 'emailRepetido') {
+  $('#avisoToast').text('Email já utilizado para cadastro')
+  $('.toast').toast('show');
+
+  setTimeout(() => {
+    $('.toast').toast('hide');
+  }, 5000);
+
+} else {
+  if (msg != 'semRegistro') {
+
+    $('#MenuRegistro').css('display', 'none');
+    $('#avisoToast').text('Usuário cadastrado com sucesso.')
+    $('.toast').toast('show');
+
+    $('#emailRegistroLogin').val('');
+    $('#senhaRegistroLogin').val('');
+    $('#nomeRegistroExibicao').val('');
+
+    setTimeout(() => {
+      $('.toast').toast('hide');
+    }, 5000);
+
+  } else {
+    $('#avisoToast').text('Erro ao inserir cadastro.')
+    $('.toast').toast('show');
+
+    setTimeout(() => {
+      $('.toast').toast('hide');
+    }, 5000);
+  }
+}
+})
+})
+
+$(document).on('click', '#fechaBatePapo', function(e){
+$('#batepapoLateral').css('display', 'none')
+$('#ReabreBatePapo').css('display', 'block')
+})
+
+$(document).on('click', '#ReabreBatePapo', function(e){
+$('#batepapoLateral').css('display', 'block')
+$('#ReabreBatePapo').css('display', 'none')
 })
